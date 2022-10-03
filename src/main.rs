052ab025c2e0e1125aaf5fa4d9ed758094aabf8c
psdev30 <psdev3012@gmail.com>
2022-10-03 00:16:48 -0400

    added missing comma

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         "gavin_credit.png",
         "dan_credit.png",
 	    "camryn_credit.png",
-        "prateek_credit.png"
         "caela_credit.png",
+        "prateek_credit.png",
     ];
 
     for i in 0..slides.len() {
