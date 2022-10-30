32738d8e66ae94f3436bd92596ca8374124de22c
Caela Go <cmg139@pitt.edu>
2022-10-30 18:17:24 -0400

    Standardized start menu buttons and added multiplayer option

--- src/start_menu.rs
+++ src/start_menu.rs
@@ -11,17 +11,13 @@ use crate::backgrounds::{
 	Tile
 };
 
-//for now, text for buttons is black, but that can be changed here:
-const TEXT_COLOR: Color = Color::rgb(0.,0.,0.);
 const MENU_BACKGROUND: &str = "backgrounds/start_screen.png";
+const TEXT_COLOR: Color = Color::rgb(0.9,0.9,0.9);
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.75, 0.35, 0.35);
 
 pub struct MainMenuPlugin;
-//sets up Handlers for fonts and images for a button.
-struct UiAssets{
-	font: Handle<Font>,
-	button: Handle<Image>,
-	button_pressed: Handle<Image>,
-}
 
 #[derive(Component)]
 pub(crate) struct MainMenuBackground;
@@ -31,6 +27,12 @@ pub(crate) struct StartButton;
 
 #[derive(Component)]
 pub(crate) struct CreditsButton;
+
+#[derive(Component)]
+pub(crate) struct MultiplayerButton;
+
+#[derive(Component)]
+pub(crate) struct StartMenuUIElement;
 
 //Builds plugin called MainMenuPlugin
 impl Plugin for MainMenuPlugin {
@@ -42,6 +44,7 @@ impl Plugin for MainMenuPlugin {
 			.run_in_state(GameState::Start)
 				.with_system(start_button_handler)
 				.with_system(credits_button_handler)
+				.with_system(multiplayer_button_handler)
 			.into())
 		.add_exit_system(GameState::Start, despawn_start_menu);
 	}
@@ -68,209 +71,217 @@ fn despawn_start_menu(mut commands: Commands,
 	}
 }
 
-fn start_button_handler(
-	mut commands: Commands,
-	interaction_query: Query<(&Children, &Interaction, With<StartButton>), Changed<Interaction>>,
-	mut image_query: Query<&mut UiImage>, 
-	ui_assets: Res<UiAssets>,
-){
-	for(children, interaction, _) in interaction_query.iter() {
-		//grabs children of button
-		let child = children.iter().next().unwrap();
-		//gets image of buttons
-		let mut image = image_query.get_mut(*child).unwrap();
-
-		//What happens when a button is interacted with
-		match interaction {
-			Interaction::Clicked =>{
-				image.0 = ui_assets.button_pressed.clone();
-				commands.insert_resource(NextState(GameState::StartPlaying));
-			},
-			Interaction::Hovered=> {
-				image.0 = ui_assets.button_pressed.clone();
-			}
-			Interaction::None => {
-				image.0 = ui_assets.button.clone();
-			}
-		}
-	}
-}
-
-fn credits_button_handler(
-	mut commands: Commands,
-	interaction_query: Query<(&Children, &Interaction, With<CreditsButton>), Changed<Interaction>>,
-	mut image_query: Query<&mut UiImage>, 
-	ui_assets: Res<UiAssets>,
+pub (crate) fn start_button_handler(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<StartButton>),
+    >,
+    mut text_query: Query<&mut Text>,
+    mut commands: Commands
 ) {
-	for(children, interaction, _) in interaction_query.iter() {
-		//grabs children of button
-		let child = children.iter().next().unwrap();
-		//gets image of buttons
-		let mut image = image_query.get_mut(*child).unwrap();
 
-		//What happens when a button is interacted with
-		match interaction {
-			Interaction::Clicked =>{
-				image.0 = ui_assets.button_pressed.clone();
-				// Change to the credits state
-				commands.insert_resource(NextState(GameState::Credits));
-			},
-			Interaction::Hovered=> {
-				image.0 = ui_assets.button_pressed.clone();
-			}
-			Interaction::None => {
-				image.0 = ui_assets.button.clone();
-			}
-		}
-	}
+    for (interaction, mut color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(*children.iter().next().unwrap()).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "Start Game".to_string();
+                *color = PRESSED_BUTTON.into();
+                commands.insert_resource(NextState(GameState::StartPlaying));
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Start Game".to_string();
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                text.sections[0].value = "Start Game".to_string();
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
 }
 
+pub (crate) fn credits_button_handler(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<CreditsButton>),
+    >,
+    mut text_query: Query<&mut Text>,
+    mut commands: Commands
+) {
+
+    for (interaction, mut color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(*children.iter().next().unwrap()).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "Credits".to_string();
+                *color = PRESSED_BUTTON.into();
+                commands.insert_resource(NextState(GameState::Credits));
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Credits".to_string();
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                text.sections[0].value = "Credits".to_string();
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+pub (crate) fn multiplayer_button_handler(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<MultiplayerButton>),
+    >,
+    mut text_query: Query<&mut Text>,
+    mut commands: Commands
+) {
+
+    for (interaction, mut color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(*children.iter().next().unwrap()).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "Multiplayer".to_string();
+                *color = PRESSED_BUTTON.into();
+                // commands.insert_resource(NextState(GameState::StartPlaying));
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Multiplayer".to_string();
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                text.sections[0].value = "Multiplayer".to_string();
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
 
 fn setup_menu(mut commands: Commands,
-	assets: Res<AssetServer>,
+	asset_server: Res<AssetServer>,
 	cameras: Query<Entity, (With<Camera2d>, Without<MenuCamera>, Without<Player>, Without<Tile>)>
 ){ 
 	cameras.for_each(|camera| {
 		commands.entity(camera).despawn();
 	});
 
-	//TODO:
-	//Choose actual font and button images
-	//gives font and images for start button:
-	let ui_assets = UiAssets {
-		font: assets.load("buttons/joystix monospace.ttf"),
-		button: assets.load("buttons/start_button.png"),
-		button_pressed: assets.load("buttons/start_button_pressed.png"),
-	};
-
 	//creates camera for UI
 	let camera = Camera2dBundle::default();
     commands.spawn_bundle(camera).insert(MenuCamera);
 
 	commands.spawn_bundle(SpriteBundle {
-		texture: assets.load(MENU_BACKGROUND),
+		texture: asset_server.load(MENU_BACKGROUND),
 		transform: Transform::from_xyz(0., 0., 0.),
 		..default()
 	})
 	.insert(MainMenuBackground);
 	
-	//START BUTTON:
-	//Note that the button comes in two parts: the clickable part and the image part.
-	//The image part will be a child of the clickable part.
-	//CLICKABLE PART OF START BUTTON:
-	commands.spawn_bundle(ButtonBundle {
-		//sets up Style for button so that it's in the center of the screen.
-		//documentation for Style can be found here: https://docs.rs/bevy/0.1.2/bevy/prelude/struct.Style.html
-		style: Style {  
-			//aligns self and children in center
-			align_items: AlignItems::Center, 
-			align_self: AlignSelf::Center, 
-			justify_content: JustifyContent::Center,
-			margin: UiRect::all(Val::Auto),
-			size: Size::new(Val::Percent(40.), Val::Percent(20.)), 
-			..Default::default()
+
+	// START BUTTON
+	commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(300.0), Val::Px(65.0)),
+                // center button
+                margin: UiRect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+			// 	position_type: PositionType::Absolute,
+			// 	position: UiRect {
+			// 	bottom: Val::Px(175.),
+			// 	left: Val::Px((WIN_W * 0.8) / 2.),
+			// 	..default()
+			// },
+			..default()
 		},
-		//makes clickable part invisible so that you can see the image part.
-		color: Color::NONE.into(),
-		..Default::default()
-	})
-	//IMAGE PART OF START BUTTON:
-	.with_children(|parent| {
-		parent.spawn_bundle(ImageBundle
-		{
-			style: Style {
-				size: Size::new(Val::Percent(100.), Val::Percent(100.)),
-				justify_content: JustifyContent::Center,
-				align_items: AlignItems::Center,
-				..Default::default()
-			},
-			image: ui_assets.button.clone().into(),
-			..Default::default()
-		})
-		//Prevents image of button from blocking the button for a mouse click.
-		.insert(FocusPolicy::Pass)
-		//Creates the TEXT on the button
-		.with_children(|parent| 
-		{
-			parent.spawn_bundle( TextBundle 
-			{
-				text: Text::from_section
-				(
-					//Text on the START Button
-					"Enter the Wastes",
-					TextStyle {
-						font: ui_assets.font.clone(),
-						font_size: 40.,
-						color: TEXT_COLOR,
-						..Default::default()
-					},
-				),
-				focus_policy: FocusPolicy::Pass,
-				..Default::default()
-			});
-		});
-	}).insert(StartButton);
+            color: NORMAL_BUTTON.into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle::from_section(
+                "Start Game",
+                TextStyle {
+                    font: asset_server.load("buttons/joystix monospace.ttf"),
+                    font_size: 40.0,
+                    color: TEXT_COLOR,
+                },
+            ));
+        })
+        .insert(StartButton)
+        .insert(StartMenuUIElement);
+	
 
-
-	//CREDITS button:
-	commands.spawn_bundle(ButtonBundle {
-			style: Style {  
-			align_items: AlignItems::Center, 
-			align_self: AlignSelf::Center, 
-			justify_content: JustifyContent::Center,
+	// CREDITS BUTTON
+	commands
+	.spawn_bundle(ButtonBundle {
+		style: Style {
+			size: Size::new(Val::Px(225.0), Val::Px(65.0)),
+			// center button
 			margin: UiRect::all(Val::Auto),
-			//button needs to be absolutely aligned
+			// horizontally center child text
+			justify_content: JustifyContent::Center,
+			// vertically center child text
+			align_items: AlignItems::Center,
 			position_type: PositionType::Absolute,
 			position: UiRect {
-				bottom: Val::Px(100.),
-				left: Val::Px((WIN_W * 0.8) / 2.),
+				bottom: Val::Px(170.),
+				left: Val::Px((WIN_W * 0.825) / 2.),
 				..default()
 			},
-			//CREDITS button is smaller than START button:
-			size: Size::new(Val::Percent(20.), Val::Percent(10.)), 
-			..Default::default()
+			..default()
 		},
-		color: Color::NONE.into(),
-		..Default::default()
+		color: NORMAL_BUTTON.into(),
+		..default()
 	})
-	//IMAGE PART of CREDITS button:
 	.with_children(|parent| {
-		parent.spawn_bundle(ImageBundle
-		{
-			style: Style {
-				size: Size::new(Val::Percent(100.), Val::Percent(100.)),
-				justify_content: JustifyContent::Center,
-				align_items: AlignItems::Center,
-				..Default::default()
+		parent.spawn_bundle(TextBundle::from_section(
+			"Credits",
+			TextStyle {
+				font: asset_server.load("buttons/joystix monospace.ttf"),
+				font_size: 40.0,
+				color: TEXT_COLOR,
 			},
-			image: ui_assets.button.clone().into(),
-			..Default::default()
-		})
-		.insert(FocusPolicy::Pass)
-		//TEXT part of CREDITS button
-		.with_children(|parent| 
-		{
-			parent.spawn_bundle( TextBundle 
-			{
-				text: Text::from_section
-				(
-					//Text on the START Button
-					"Credits",
-					TextStyle {
-						font: ui_assets.font.clone(),
-						//font size of credits must be smaller than START
-						font_size: 30.,
-						color: TEXT_COLOR,
-						..Default::default()
-					},
-				),
-				focus_policy: FocusPolicy::Pass,
-				..Default::default()
-			});
-		});
-	}).insert(CreditsButton);
-	//END CREDITS BUTTON
+		));
+	})
+	.insert(CreditsButton)
+	.insert(StartMenuUIElement);
 
-	//adds resources to the App
-	commands.insert_resource(ui_assets);
+
+	// MULTIPLAYER BUTTON
+	commands
+	.spawn_bundle(ButtonBundle {
+		style: Style {
+			size: Size::new(Val::Px(325.0), Val::Px(65.0)),
+			// center button
+			margin: UiRect::all(Val::Auto),
+			// horizontally center child text
+			justify_content: JustifyContent::Center,
+			// vertically center child text
+			align_items: AlignItems::Center,
+			position_type: PositionType::Absolute,
+			position: UiRect {
+				bottom: Val::Px(250.),
+				left: Val::Px((WIN_W * 0.75) / 2.),
+				..default()
+			},
+			..default()
+		},
+		color: NORMAL_BUTTON.into(),
+		..default()
+	})
+	.with_children(|parent| {
+		parent.spawn_bundle(TextBundle::from_section(
+			"Multiplayer",
+			TextStyle {
+				font: asset_server.load("buttons/joystix monospace.ttf"),
+				font_size: 40.0,
+				color: TEXT_COLOR,
+			},
+		));
+	})
+	.insert(MultiplayerButton)
+	.insert(StartMenuUIElement);
 }
